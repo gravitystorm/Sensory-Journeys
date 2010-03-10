6c2beb01365a0aa195ef00d5a4991079244986ca
Andy Allan <andy@gravitystorm.co.uk>
2010-03-10 17:48:35 +0000

    stash this quick

--- web/app/controllers/trace_controller.rb
+++ web/app/controllers/trace_controller.rb
@@ -1,4 +1,7 @@
 class TraceController < ApplicationController
+  require 'xml/libxml'
+  include XML
+  
   def index
     @traces = Trace.find(:all)
   end
@@ -7,7 +10,25 @@ class TraceController < ApplicationController
   end
   
   def uploadFile
-    flash[:notice] = "Not implemented"
+    t = Trace.new()
+    t.file_name = params[:upload]['gpx'].original_filename
+    t.save!
+    
+    parser = XML::Parser.new()
+    parser.string = params[:upload]['gpx'].read
+    doc = parser.parse
+    
+    doc.find('//g:trkpt', 'g:http://www.topografix.com/GPX/1/1').each do |trkpt|
+      pt = TracePoint.new()
+      pt.lat = trkpt["lat"]
+      pt.lon = trkpt["lon"]
+      pt.timestampe = trkpt["timestamp"]
+      pt.trace_id = t.id
+      pt.save!
+    end
+    
+    flash[:notice] = "foo"
+    #redirect_to(:action => :view, :id => t.id)
     redirect_to(:action => :upload)
   end
 end
