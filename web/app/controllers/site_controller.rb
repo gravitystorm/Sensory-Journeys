6242b5e62ea9055a4cc9d631118d238d679f3207
Andy Allan <andy@gravitystorm.co.uk>
2010-03-25 14:09:41 +0000

    Start getting login/logout things in the right places

--- web/app/controllers/site_controller.rb
+++ web/app/controllers/site_controller.rb
@@ -2,5 +2,19 @@ class SiteController < ApplicationController
   def index
     @schools = School.find(:all)
   end
+  
+  def edit
+  end
 
+  def login
+    #TODO do stuff
+    flash[:notice] = "Login functionality not yet implemented"
+    redirect_to(:action => :edit)
+  end
+  
+  def logout
+    #TODO do stuff
+    flash[:notice] = "You are now logged out"
+    redirect_to(:action => :index)
+  end
 end
