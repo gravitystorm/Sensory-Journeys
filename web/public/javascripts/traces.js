3f8ca9bbada872fe2633903c26eb2012388ca18c
Andy Allan <andy@gravitystorm.co.uk>
2010-04-15 12:01:05 +0100

    provide function for getting traces by mode

--- web/public/javascripts/traces.js
+++ web/public/javascripts/traces.js
@@ -1,13 +1,20 @@
-var traces = new OpenLayers.Layer.GML("traces", "/trace/traces.kml",
-                {   format: OpenLayers.Format.KML,
-                    projection: new OpenLayers.Projection("EPSG:4326"),
-                    formatOptions: {
-                      extractStyles: false,
-                      extractAttributes: true,
-                      maxDepth: 2
-                    },
-                  strategies: [new OpenLayers.Strategy.BBOX()],
-                  projection: new OpenLayers.Projection("EPSG:4326"),
-                  styleMap: new OpenLayers.StyleMap({ strokeWidth: 9, strokeColor: '#0000ff', strokeOpacity: 0.7 }),
-                  minScale: 100000
-                });
+
+function getTraces(mode) {
+  if(mode) {
+    url = '/trace/traces.kml?mode='+mode
+  } else {
+    url = '/trace/traces.kml'
+  }
+
+  layer = new OpenLayers.Layer.Vector("traces",
+            { protocol: new OpenLayers.Protocol.HTTP({
+              url: url,
+              format: new OpenLayers.Format.KML({extractStyles: false, extractAttributes: true}),
+            }),
+            strategies: [new OpenLayers.Strategy.BBOX()],
+            projection: new OpenLayers.Projection("EPSG:4326"),
+            styleMap: new OpenLayers.StyleMap({ strokeWidth: 9, strokeColor: '#0000ff', strokeOpacity: 0.7 }),
+            minScale: 100000
+            });
+  return layer;
+}
