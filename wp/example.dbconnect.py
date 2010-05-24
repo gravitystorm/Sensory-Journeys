a5365721b8f9ff964735c4fe9e9da748bb0cccb8
Andy Allan <andy@gravitystorm.co.uk>
2010-05-24 20:17:39 +0100

    Composite scans into different layers.

---
+++ wp/example.dbconnect.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Change these values, and save as dbconnect.py
+
+dbconnection = "host='localhost' dbname='sensory_dev' user='sensory' password='passwerd'"
--- wp/scans-compile.py
+++ wp/scans-compile.py
@@ -6,42 +6,64 @@
 
 import os, shutil, sys
 from PIL import Image
+import psycopg2
+
+import dbconnect
 
 base = os.path.join(os.getcwd(), "site", "www", "files", "scans")
 #base = "/home/andy/src/sustrans/wp/site/www/files/scans/"
 #base = "/home/andy/temp/sustrans/scans/"
 #path, scans, = os.walk(path)
 
+def composite(base, scan, target):
+  print "\nCompositing %s onto %s" % (scan, target)
+  for zoom in os.listdir(os.path.join(base, scan)):
+    if os.path.isdir(os.path.join(base, scan, zoom)):
+      #print zoom
+      for x in os.listdir(os.path.join(base, scan, zoom)):
+        for y in os.listdir(os.path.join(base, scan, zoom, x)):
+          if os.path.exists(os.path.join(base, target, zoom, x, y)): #blend the tiles together
+            im = Image.open(os.path.join(base, target, zoom, x, y))
+            im2 = Image.open(os.path.join(base, scan, zoom, x, y))
+            im.paste(im2, None, im2)
+            im.save(os.path.join(base, target, zoom, x, y))
+            sys.stdout.write(".")
+            sys.stdout.flush()
+          else: #copy the file over as-is
+            try:
+              os.makedirs(os.path.join(base, target, zoom, x))
+            except:
+              pass #directory already exists
+            shutil.copy(os.path.join(base, scan, zoom, x, y), os.path.join(base, target, zoom, x))
+            sys.stdout.write("C")
+            sys.stdout.flush()
+            
+conn = psycopg2.connect(dbconnect.dbconnection)
+cur = conn.cursor()
 
-all = "all"
 
+# composite all scans
+cur.execute("Select scan_id from shadow_scans")
+scans = cur.fetchall()
+all = "composite/all"
 try:
   shutil.rmtree(os.path.join(base, all))
 except:
   pass
+for scan in scans:
+  composite(base, scan[0], all)
 
-for scan in os.listdir(base):
-  print "\n %s" % scan
-  if scan == "all":
-    continue
-  if os.path.isdir(os.path.join(base, scan)):
-    for zoom in os.listdir(os.path.join(base, scan)):
-      if os.path.isdir(os.path.join(base, scan, zoom)):
-        #print zoom
-        for x in os.listdir(os.path.join(base, scan, zoom)):
-          for y in os.listdir(os.path.join(base, scan, zoom, x)):
-            if os.path.exists(os.path.join(base, all, zoom, x, y)): #blend the tiles together
-              im = Image.open(os.path.join(base, all, zoom, x, y))
-              im2 = Image.open(os.path.join(base, scan, zoom, x, y))
-              im.paste(im2, None, im2)
-              im.save(os.path.join(base, all, zoom, x, y))
-              sys.stdout.write(".")
-              sys.stdout.flush()
-            else: #copy the file over as-is
-              try:
-                os.makedirs(os.path.join(base, all, zoom, x))
-              except:
-                pass #already exists
-              shutil.copy(os.path.join(base, scan, zoom, x, y), os.path.join(base, all, zoom, x))
-              sys.stdout.write("C")
-              sys.stdout.flush()
+# composite each mode
+cur.execute("Select distinct mode_id from shadow_scans")
+modes = cur.fetchall()
+for mode in modes:
+  mode_path = "composite/mode_%i" % mode
+  try:
+    shutil.rmtree(os.path.join(base, mode_path))
+  except:
+    pass
+  cur.execute("select scan_id from shadow_scans where mode_id = %s" % mode)
+  scans = cur.fetchall()
+  for scan in scans:
+    composite(base, scan[0], mode_path)
+
