1f52c374207974b6ac52b2920852611c4385ec85
Andy Allan <andy@gravitystorm.co.uk>
2010-05-26 15:18:27 +0100

    Add default scope ordering for schools and modes

--- web/app/models/mode.rb
+++ web/app/models/mode.rb
@@ -1,4 +1,7 @@
 class Mode < ActiveRecord::Base
   has_many :traces
   has_many :shadow_scans
+  
+  default_scope :order => :id
+  
 end
--- web/app/models/school.rb
+++ web/app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
   has_many :traces
   has_many :shadow_scans
+  
+  default_scope :order => :id
+  
 end
