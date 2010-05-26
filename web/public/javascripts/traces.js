810c35e751e4024865d59a9ab101308f0c9889ad
Andy Allan <andy@gravitystorm.co.uk>
2010-05-26 18:31:53 +0100

    Show waypoints on the view page too

--- web/public/javascripts/traces.js
+++ web/public/javascripts/traces.js
@@ -13,7 +13,7 @@ function getTraces(mode) {
             }),
             strategies: [new OpenLayers.Strategy.BBOX()],
             projection: new OpenLayers.Projection("EPSG:4326"),
-            styleMap: new OpenLayers.StyleMap({ strokeWidth: 4, strokeColor: '#0000ff', strokeOpacity: 0.4 }),
+            styleMap: new OpenLayers.StyleMap({strokeWidth: 4, strokeColor: '#0000ff', strokeOpacity: 0.4, pointRadius: 5, fillColor: '#ff00ff', fillOpacity: 0.4}),
             minScale: 100000
             });
   return layer;
