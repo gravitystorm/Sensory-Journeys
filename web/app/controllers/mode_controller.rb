aa36b0b71e8d1d7f46e7d71436473bda64b9e6ab
Andy Allan <andy@gravitystorm.co.uk>
2010-06-01 17:20:29 +0100

    Add and delete modes and schools

--- web/app/controllers/mode_controller.rb
+++ web/app/controllers/mode_controller.rb
@@ -1,5 +1,29 @@
 class ModeController < ApplicationController
 
+  before_filter :authorize
+  before_filter :require_admin
+  
   in_place_edit_for :mode, :name
+  
+  def add
+    m = Mode.new
+    m.name = params[:name]
+    m.save!
+    
+    flash[:notice] = "Added new mode"
+    redirect_to(:controller => :admin, :action => :modes)
+  end
+  
+  def delete
+    m = Mode.find_by_id(params[:mode_id])
+    if m.traces.count == 0 && m.shadow_scans.count == 0
+      m.destroy
+      flash[:notice] = "Mode deleted"
+    else
+      flash[:error] = "Mode has either traces or scans, so not deleted"
+    end
+    
+    redirect_to(:controller => :admin, :action => :modes)
+  end
 
 end
--- web/app/controllers/school_controller.rb
+++ web/app/controllers/school_controller.rb
@@ -1,7 +1,32 @@
 class SchoolController < ApplicationController
 
+  before_filter :authorize
+  before_filter :require_admin
+  
   in_place_edit_for :school, :name
   in_place_edit_for :school, :lat
   in_place_edit_for :school, :lon
 
+  def add
+    s = School.new
+    s.name = params[:name]
+    s.lat = params[:lat]
+    s.lon = params[:lon]
+    s.save!
+    
+    flash[:notice] = "Added new school"
+    redirect_to(:controller => :admin, :action => :schools)
+  end
+  
+  def delete
+    s = School.find_by_id(params[:school_id])
+    if s.traces.count == 0 && s.shadow_scans.count == 0
+      s.destroy
+      flash[:notice] = "School deleted"
+    else
+      flash[:error] = "School has either traces or scans, so not deleted"
+    end
+    
+    redirect_to(:controller => :admin, :action => :schools)
+  end
 end
