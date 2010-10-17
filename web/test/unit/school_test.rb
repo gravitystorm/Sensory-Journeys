d5a1a60b01aec6ca7208a1ca43ed79de06c65f41
Andy Allan <andy@gravitystorm.co.uk>
2010-10-17 16:03:07 +0100

    Test lat, lon and project are available on schools.

--- web/test/unit/school_test.rb
+++ web/test/unit/school_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class SchoolTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :schools, :projects
+  
+  def test_fixtures
+    assert_equal 2, School.count
+  end
+
+  test "should not save school without lat/lon" do
+    school = School.new(:project => Project.find(:first))
+    assert !school.save, "Saved the school without lat/lon"
+    school.lat = "51.2"
+    assert !school.save, "Saved the school with only lat"
+    school.lon = "0.2"
+    assert school.save
+  end
+
+  test "should not save school without project" do
+    school = School.new(:lat => "40", :lon => "45")
+    assert !school.save, "Saved school without project"
+    school.project = Project.find(:first)
+    assert school.save
   end
 end
